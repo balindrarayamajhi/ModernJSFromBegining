7836a090ebb022fe350b7dd33541eb1f08b07eee
Balindra <balindrarayamajhi@gmail.com>
2019-08-15 13:40:31 -0400

    Initail commit

---
+++ index.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>client side JS</title>
+        
+    <body>
+         <script type="text/javascript" src="testfile"></script>
+       
+    </body>
+</html>
